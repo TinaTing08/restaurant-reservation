55ed7827f6a65e8fe9acffed83a01e436236f0bd
TinaTing08 <94713998+TinaTing08@users.noreply.github.com>
2026-03-12 13:37:58 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Restaurant Reservation Website"
+    return "Restaurant Reservation Website is running!"
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
